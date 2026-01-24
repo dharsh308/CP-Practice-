b91d3fd9ebb422414896b73ff0bebcbd57dd3ba4
mrhoax308 <dharsh308@gmail.com>
2026-01-24 22:01:44 +0530

    Solved LC 3 - Longest substring without repeating characters

---
+++ LeetCode/Medium/3_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, int> freq;
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.size(); ++right)
+        {
+            freq[s[right]]++;
+
+            while (freq[s[right]] > 1)
+            {
+                freq[s[left]]--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
