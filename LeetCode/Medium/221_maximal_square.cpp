156229eeb05e2c526c1f85846530c4f106e2a077
mrhoax308 <dharsh308@gmail.com>
2026-01-27 22:46:52 +0530

    Solved LC 221 - Maximal square

---
+++ LeetCode/Medium/221_maximal_square.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximalSquare(vector<vector<char>> &matrix)
+    {
+        int n = matrix.size(), m = matrix[0].size(), maxL = 0;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0] = (matrix[0][0] == '1');
+        maxL = dp[0][0];
+        for (int i = 1; i < n; i++)
+        {
+            dp[i][0] = (matrix[i][0] == '1');
+            maxL = max(maxL, dp[i][0]);
+        }
+        for (int j = 1; j < m; j++)
+        {
+            dp[0][j] = (matrix[0][j] == '1');
+            maxL = max(maxL, dp[0][j]);
+        }
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[i][j] == '1')
+                {
+                    dp[i][j] =
+                        min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) +
+                        1;
+                    maxL = max(maxL, dp[i][j]);
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return maxL * maxL;
+    }
+};
