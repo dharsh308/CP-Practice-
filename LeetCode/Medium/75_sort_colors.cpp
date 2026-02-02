ade83b6339a778262004a0d758a684df8d5789a9
mrhoax308 <dharsh308@gmail.com>
2026-02-02 22:47:29 +0530

    Solved LC 75 - Sort Colors

---
+++ LeetCode/Medium/75_sort_colors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int i = 0, j = 0, k = nums.size() - 1;
+        while (j <= k)
+        {
+            switch (nums[j])
+            {
+            case 0:
+                swap(nums[j], nums[i]);
+                j++;
+                i++;
+                break;
+
+            case 1:
+                j++;
+                break;
+            case 2:
+                swap(nums[j], nums[k]);
+                k--;
+                break;
+            }
+        }
+    }
+};
