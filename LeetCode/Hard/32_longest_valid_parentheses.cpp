e855d7b97a809854fa7aee545ced80d40dd71ee3
mrhoax308 <dharsh308@gmail.com>
2026-02-11 23:00:27 +0530

    Solved LC 32 - Longest valid parentheses

---
+++ LeetCode/Hard/32_longest_valid_parentheses.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestValidParentheses(string s)
+    {
+        stack<int> st;
+        st.push(-1);
+        int maxLen = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            char ch = s[i];
+            if (ch == '(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if (st.empty())
+                    st.push(i);
+                else
+                {
+                    maxLen = max(maxLen, i - st.top());
+                }
+            }
+        }
+        return maxLen;
+    }
+};
