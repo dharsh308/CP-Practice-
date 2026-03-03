79b67bca70225c842d5b89f95296cafd9a1dd473
mrhoax308 <dharsh308@gmail.com>
2026-03-03 22:24:44 +0530

    Solved LC 150 - Evaluate reverse polish notation

---
+++ LeetCode/Medium/150_evaluate_reverse_polish_notation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int>s;
+        for(string i:tokens){
+            if(i=="*"||i=="/"||i=="-"||i=="+"){
+                int b=s.top();
+                s.pop();
+                int a=s.top();
+                s.pop();
+                if(i=="+")s.push(a+b);
+                else if(i=="*")s.push(a*b);
+                if(i=="-")s.push(a-b);
+                if(i=="/")s.push(a/b);
+            }
+            else{
+                s.push(stoi(i));
+            }
+        }
+        return s.top();
+    }
+};
