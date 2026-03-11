b67dad7a81fa879ac747dab523e927ba108c81d2
mrhoax308 <dharsh308@gmail.com>
2026-03-11 22:47:54 +0530

    Solved LC 295 - Find median from data stream

---
+++ LeetCode/Hard/295_find_median_from_data_stream.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class MedianFinder {
+public:
+    priority_queue<int> left;                             // max heap
+    priority_queue<int, vector<int>, greater<int>> right; // min
+    MedianFinder() {}
+
+    void addNum(int num) {
+        left.push(num);
+
+        right.push(left.top());
+        left.pop();
+
+        if (left.size() < right.size()) {
+            left.push(right.top());
+            right.pop();
+        }
+    }
+
+    double findMedian() {
+        if (left.size() == right.size()) {
+            return (left.top() + right.top()) / 2.0;
+        }
+        return left.top();
+    }
+};
