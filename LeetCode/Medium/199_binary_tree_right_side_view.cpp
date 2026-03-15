c7a1357038f1e1e100ea39b9f661d88b8769ecd1
mrhoax308 <dharsh308@gmail.com>
2026-03-15 20:34:53 +0530

    SOlved LC 199 - Binary tree right side view

---
+++ LeetCode/Medium/199_binary_tree_right_side_view.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include<queue>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>result;
+        if (!root)
+            return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> currentLevel;
+
+            for (int i = 0; i < levelSize; ++i) {
+                TreeNode* current = q.front();
+                q.pop();
+                currentLevel.push_back(current->val);
+
+                if (current->left)
+                    q.push(current->left);
+                if (current->right)
+                    q.push(current->right);
+            }
+            result.push_back(currentLevel.back());
+        }
+        return result;
+    }
+};
