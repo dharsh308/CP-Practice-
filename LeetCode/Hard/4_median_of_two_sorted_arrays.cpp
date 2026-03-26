e404a6d3a8c12bb7d527615c487be43f21341481
mrhoax308 <dharsh308@gmail.com>
2026-03-26 20:30:51 +0530

    Solved LC 4 - Median of two sorted arrays

--- LeetCode/Hard/4_median_of_two_sorted_arrays.cpp
+++ LeetCode/Hard/4_median_of_two_sorted_arrays.cpp
@@ -8,21 +8,36 @@ class Solution
 public:
     double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
     {
-        vector<int> v;
-        for (int i = 0; i < nums1.size(); i++)
-        {
-            v.push_back(nums1[i]);
+         int n = nums1.size();
+        int m = nums2.size();
+        if (n > m)
+            return findMedianSortedArrays(nums2, nums1);
+        int l = 0;
+        int u = n;
+
+        while (l <= u) {
+            int partitionX = l + (u - l) / 2;
+            int partitionY = (n + m + 1) / 2 - partitionX;
+
+            int leftMaxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+            int leftMaxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+            int rightMinX = (partitionX == n) ? INT_MAX : nums1[partitionX];
+            int rightMinY = (partitionY == m) ? INT_MAX : nums2[partitionY];
+
+            if (leftMaxX <= rightMinY && leftMaxY <= rightMinX) {
+                if ((n + m) % 2 == 0)
+                    return (max(leftMaxX, leftMaxY) +
+                            min(rightMinX, rightMinY)) /
+                           2.0;
+                else {
+                    return max(leftMaxX, leftMaxY);
+                }
+            } else if (leftMaxX > rightMinY)
+                u = partitionX - 1;
+            else {
+                l = partitionX + 1;
+            }
         }
-        for (int i = 0; i < nums2.size(); i++)
-        {
-            v.push_back(nums2[i]);
-        }
-        sort(v.begin(), v.end());
-        int p = (v.size() - 1) / 2;
-        if (v.size() % 2 == 0)
-        {
-            return (v[p] + v[p + 1]) / 2.0;
-        }
-        return v[p];
+        return 0.0;
     }
 };
