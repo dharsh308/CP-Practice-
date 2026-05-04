a474f0d77d7d8e72cdd37ff9c417e93e2517ec18
mrhoax308 <dharsh308@gmail.com>
2026-05-04 21:59:44 +0530

    Solved LC 25 - Reverse nodes in k group "

---
+++ LeetCode/Hard/25_reverse_nodes_in_k_group.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+    if (!head || k == 1) return head;
+
+    ListNode dummy(0);
+    dummy.next = head;
+    ListNode* prevGroupEnd = &dummy;
+
+    while (true) {
+        ListNode* kth = prevGroupEnd;
+        for (int i = 0; i < k && kth; i++) {
+            kth = kth->next;
+        }
+        if (!kth) break; 
+
+        ListNode* groupStart = prevGroupEnd->next;
+        ListNode* nextGroupStart = kth->next;
+
+        ListNode* prev = nextGroupStart;
+        ListNode* curr = groupStart;
+        while (curr != nextGroupStart) {
+            ListNode* tmp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        prevGroupEnd->next = kth;
+        prevGroupEnd = groupStart;
+    }
+
+    return dummy.next;
+}
+};
