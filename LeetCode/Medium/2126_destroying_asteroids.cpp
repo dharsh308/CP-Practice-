0883f51979bfb12b16e6278d68f3dec82d7118e9
mrhoax308 <dharsh308@gmail.com>
2026-06-10 22:41:04 +0530

    Solved LC 2126

---
+++ LeetCode/Medium/2126_destroying_asteroids.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        int n=asteroids.size();
+        sort(asteroids.begin(),asteroids.end());
+        long long total=mass;
+        for(int i:asteroids){
+            if(total>=i)total+=i;
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
