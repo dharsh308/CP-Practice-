c3ff40bb47936bee44c46a725bb2b7f175ab85d4
mrhoax308 <dharsh308@gmail.com>
2026-06-11 21:54:00 +0530

    Solved LC 2144

---
+++ LeetCode/Easy/2144_minimum_cost_to_buy_candies_with_discount.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.rbegin(), cost.rend());
+        int totalCost = 0;
+        for (int i = 0; i < cost.size(); i++) {
+            if (i % 3 != 2)
+                totalCost += cost[i];
+        }
+        return totalCost;
+    }
+};
