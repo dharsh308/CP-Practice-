635388367fd7a4b3ec738ea4a28fdcced79d2983
dharsh308 <dharsh308@gmail.com>
2026-07-09 23:06:25 +0530

    Solved LC 111

---
+++ LeetCode/Easy/111_minimum_depth_of_binary_tree.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        if (root->left == nullptr && root->right==nullptr)
+            return 1;
+        int left = INT_MAX, right = INT_MAX;
+        if (root->left)
+            left = minDepth(root->left);
+        if (root->right)
+            right = minDepth(root->right);
+        return min(left, right)+1 ;
+    }
+};
